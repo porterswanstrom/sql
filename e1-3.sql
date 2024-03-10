071126a720a1a3de4b2b553a47dbf8558b8863be
ps <ps@vern.cc>
2024-03-10 21:03:38 +0000

    1-3

---
+++ e1-3.sql
@@ -0,0 +1,8 @@
+SELECT city, state_province, country_id
+FROM locations;
+
+SELECT last_name "Client", email "Email Address"
+FROM d_clients;
+
+SELECT last_name "Last name", salary * 1.05 + .50 "Salary"
+FROM f_staffs;
